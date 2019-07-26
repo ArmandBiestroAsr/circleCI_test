57793fb82744449857ce177566e055c73ab5a51b
Armand <Armand@MacBook-Pro-de-Anais.local>
2019-07-26 11:09:30 +0200

    test_circle_1

---
+++ src/main/java/circleCI/hello.java
@@ -0,0 +1,15 @@
+package circleCI;
+
+public class hello {
+	
+		 
+		@SuppressWarnings("resource")
+		public static void main(String[] args) {
+	 
+			
+	 
+			String message = "hello";
+			System.out.println(message);
+		}
+	
+}
